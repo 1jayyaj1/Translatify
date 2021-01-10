64a9586635f6e37b33db309ae4cf824ab78d63fd
Jay Abi-Saad <jayabisaad@gmail.com>
2021-01-10 15:47:20 -0500

    Added placegolder fake API call to not use limit and is now printing correct before and after width and height of each html element

--- js/index.js
+++ js/index.js
@@ -14,20 +14,21 @@ function buildTranslationObject(elementText){
             "q": elementText,
             "target": "fr"
           }
-            
         }
      return settings;
 }
 
 function fetchTranslation(currElement, elementText){
      var settings = buildTranslationObject(elementText);
-     $.ajax(settings).done(function (response) {
+     currElement.textContent = "alloo loll cest drole jecris juste de la marde pour que ca prenne de la place en tabarnak lololol";
+     console.log(currElement.textContent);
+     //$.ajax(settings).done(function (response) {
                
-          console.log(response);
-          var translatedText = response.data.translations[0].translatedText;
-          currElement.textContent = translatedText;
+          //console.log(response);
+          //var translatedText = response.data.translations[0].translatedText;
+          //currElement.textContent = translatedText;
                
-     });
+     //});
 }
 
 var all = document.getElementsByTagName("*");
@@ -37,15 +38,20 @@ for (var i=0, max=all.length; i < max; i++) {
      var ok = [].reduce.call(curr.childNodes, function(a, b) { return a + (b.nodeType === 3 ? b.textContent : ''); }, '');
      ok = ok.replace(/^\s+|\s+$/g, '');
      if (ok !== '' && curr.tagName !== 'TITLE') {
-          console.log(curr.tagName);
-          console.log(curr.getBoundingClientRect().height);
-          console.log(curr.getBoundingClientRect().width);
-          console.log(ok);
+          var currTag = curr.tagName;
+          var currHeight = curr.getBoundingClientRect().height;
+          var currWidth = curr.getBoundingClientRect().width;
           fetchTranslation(curr, ok);
-          console.log(curr.tagName);
-          console.log(curr.getBoundingClientRect().height);
-          console.log(curr.getBoundingClientRect().width);
+          console.log(curr.textContent);
+          var newHeight = curr.getBoundingClientRect().height;
+          var newWidth = curr.getBoundingClientRect().width;
+          curr.textContent = ok;
+          console.log(currTag);
           console.log(ok);
+          console.log(currHeight);
+          console.log(currWidth);
+          console.log(newHeight);
+          console.log(newWidth);
           console.log("");
      }
 }
